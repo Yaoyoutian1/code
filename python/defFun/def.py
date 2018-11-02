cfb029f65cf6c734b7d1a4bce86deb746ef09d5f
Yaoyoutian1 <13271945071@163.com>
2018-11-02 23:27:41 +0800

    def 函数基本操作

--- python/defFun/def.py
+++ python/defFun/def.py
@@ -13,8 +13,25 @@
 
 
 函数默认值只会初始化一次
+可选参数,关键字参数,形式参数
 
 '''
+oldage=20
+# 默认值只初始化一次
+def as_ok(name:str, age=oldage, sex="男", remarks="这个男孩来自中国")->str:
+    """
+    函数注解
+    
+    """
+    i = 1
+    while True:
+        ok = input(name)
+        if ok in ('yyt', 'y', 'yy'):
+            print("我叫姚友田是个{0},今年{1}岁,{2}".format(age, sex, remarks))
+        elif ok in ("no"):
+            print(remarks)
+        else:
+            print("hello")
 
-
-
+oldage=22
+as_ok('y')
